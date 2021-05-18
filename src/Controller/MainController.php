7ef53696aae66bced19c71020b9954cd08db5b2c
fdersu <fredericdersu@gmail.com>
2021-05-18 16:31:15 +0200

    commit 18-05-2021 fin de journée

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /** @Route("/", name="main_home") */
-    public function home(){
+    public function home():Response{
         return $this->render('main/home.html.twig');
+    }
+
+    /** @Route("/about", name="main_about") */
+    public function about():Response{
+        return $this->render('main/about.html.twig');
     }
 }
---
+++ src/Controller/WishController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    /** @Route("/list", name="wish_list") */
+    public function list():Response{
+        return $this->render('wish/list.html.twig');
+    }
+
+    /** @Route("/list/detail/{id}", name="wish_detail") */
+    public function detail($id):Response{
+        return $this->render('wish/detail.html.twig');
+    }
+}
